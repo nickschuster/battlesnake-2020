2b545aa8e8cfa4c7878be0d44bf9c34177606343
Nikolas Schuster <ngpnickschuster@gmail.com>
2020-02-19 13:11:55 -0800

    Added instructions for templating.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Battlesnake2020
-Battlesnake 2020 snake.
+# Battlesnake 2020
+Developed in node.js and hosted on aws.
+
+## Use Instructions
+1. Copy the template file and rename it to something useful.
+2. To run use 'node *name of file*.js'
+3. If hosting on aws ec2 the URL path for adding to your battlesnake profile is this: http://*ec2instance*.amazonaws.com:*port*
+4. Add logic to start, end and move endpoints.
---
+++ template.js
@@ -0,0 +1,44 @@
+// Logger
+const log = require('morgan');
+
+// Handle all incoming requests
+const express = require('express');
+const app = express();
+
+app.set('port', (9001));
+
+app.enable('verbose errors');
+
+app.use(log('dev'));
+app.use(express.json());
+
+// Make sure snake is alive (responding to requests).
+app.post('/ping', (request, response) => {
+    return response.status(200).json({});
+});
+
+// Start of a game. Return snake options.
+app.post('/start', (request, response) => {
+    const snake = {
+        color: '#DFFF00',
+        headType: 'dead',
+        tailType: 'bolt'
+    };
+    console.log('Game starting.');
+    return response.status(200).json(snake);
+});
+
+// Ask for move. Get 500 ms to repond with up,left,right,down.
+app.post('/move', (request, response) => {
+    return response.status(200).json({'move': 'left'});
+});
+
+// Game has ended.
+app.post('/end', (request, response) => {
+    return response.status(200).json({});
+});
+
+// Listen on port.
+app.listen(app.get('port'), () => {
+    console.log(`Listening: ${app.get('port')}.`);
+});
