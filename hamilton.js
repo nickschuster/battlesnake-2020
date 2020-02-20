05897e74322a063a9abe457e9082639bf3d0a880
Nikolas Schuster <ngpnickschuster@gmail.com>
2020-02-20 10:46:31 -0800

    Started on hamilton cycle gen.

--- hamilton.js
+++ hamilton.js
@@ -12,6 +12,40 @@ app.enable('verbose errors');
 app.use(log('dev'));
 app.use(express.json());
 
+// Game variables.
+const snake = {
+    color: '#DFFF00',
+    headType: 'dead',
+    tailType: 'bolt'
+};
+const cycleIndex = 0;
+const boardIndex = 0;
+let gameData = {};
+
+
+class Node {
+    constructor() {
+        this.visited = false;
+        this.canGoRight = false;
+        this.canGoLeft = false;
+    }
+}
+class Maze {
+    constructor(game) {
+        let nodes = [];
+        for(let i = 0; i < game.board.height/2; i++) {
+            nodes.push(new Node());
+        }
+    }
+}
+
+class HamCycle {
+    constructor(game) {
+        this.maze = new Maze(game);
+        this.game = game;
+    }
+}
+
 // Make sure snake is alive (responding to requests).
 app.post('/ping', (request, response) => {
     return response.status(200).json({});
@@ -19,12 +53,9 @@ app.post('/ping', (request, response) => {
 
 // Start of a game. Return snake options.
 app.post('/start', (request, response) => {
-    const snake = {
-        color: '#DFFF00',
-        headType: 'dead',
-        tailType: 'bolt'
-    };
     console.log('Game starting.');
+    console.log(request);
+    gameData[request.body.game.id] = [new HamCycle(request.body)];
     return response.status(200).json(snake);
 });
 
